971bdd1f419a843de6376e30a9ec4b0e1f00d8ec
chanakaHetti <cdileep40@gmail.com>
2024-09-08 15:45:13 +0100

    Adding the changes for handling transition between two application with port forwarding

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hello from Next application
-      </main>
+    <div>
+      <h1>Next.js Application</h1>
+      <hr />
+      <Link href="http://localhost:3002">Go to Nuxt.js App</Link>
     </div>
   );
 }
--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,3 +1,6 @@
 <template>
-  <div>Hello from Nuxt application</div>
+  <div>
+    <h1>Nuxt.js Application</h1>
+    <nuxt-link to="/next">Go to Next.js Application</nuxt-link>
+  </div>
 </template>
